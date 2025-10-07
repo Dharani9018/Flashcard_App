6a42d8f93b7133220cb43c72291a4be8cbafdce3
Jithesh Marar <diyajithumarar@gmai.com>
2025-10-07 09:22:16 +0530

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import "../css/Login.css"
 
 function Login() {
     return (
-        <div className="login">
+        <div className="login!!">
             <h2>Login</h2>
             <form>
                 <input type="email" placeholder="Enter your email" /><br />
