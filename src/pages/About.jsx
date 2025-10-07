e46f285de47cec48ac9bd8af8373cb0b5ac5db89
disha-bose-8 <dishab817@gmail.com>
2025-10-07 08:38:41 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
         This flashcard application is designed to help users learn and memorize information efficiently. Users can create, edit, and review flashcards on various topics. The app utilizes spaced repetition techniques to enhance learning and retention.
       </p>
       <p className="text-lg text-center max-w-2xl mt-4">
-        Built with modern web technologies, this app aims to provide a seamless and engaging user experience. Whether you're a student preparing for exams or someone looking to expand your knowledge, this flashcard app is here to assist you on your learning journey.
+        Built with modern web technologies, this app aims to provide a seamless and engaging user experience. Whether you're a student preparing for exams or someone looking to expand your knowledge, this flashcard app is here to assist you on your learning journey!!
       </p>
     </div>
   );
